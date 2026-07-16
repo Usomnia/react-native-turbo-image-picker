0839bf3245cc6a3272824182e5cfdb5e7f7cdaf7
mhpc0909 <mcse0325@hotmail.com>
2026-07-16 13:08:43 +0900

    Fix Android autolinking for prebuilt AAR

---
+++ android/src/main/java/com/rnturboimagepicker/DummyPackage.java
@@ -0,0 +1,17 @@
+package com.rnturboimagepicker;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+import java.util.Collections;
+import java.util.List;
+public class DummyPackage implements ReactPackage {
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
